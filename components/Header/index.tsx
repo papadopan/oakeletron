75156ffbda05ad96d9c5726fab1f27fd9b0820c0
Antonios Papadopoulos <antonios@smallpdf.com>
2023-03-14 15:44:06 +0100

    customize Header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link';
+
 export function Header() {
   return (
     <>
       <header>
-        <div>antonios</div>
+        <nav className="bg-white h-10 p-8 flex flex-row-reverse items-center">
+          <Link href="/blog" className="text-gray-700">
+            Blog
+          </Link>
+        </nav>
       </header>
     </>
   );
